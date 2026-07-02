0622e44426c310c24d2b29ef1546a7baf02b48a3
Omadbek <omadbekmakhmudov99@gmail.comgit config --listgit config --global user.email omadbekmakhmudov99@gmail.comgit config --global user.name Omadbekgit config --global user.email omadbekmkahmudov99@gmail.comgit config --listgit commit -m :Initial PrivAge API>
2026-07-02 04:02:43 +0300

    Connect MongoDB database

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log("🗄️ MongoDB connected");
+})
+.catch((error) => {
+    console.log("MongoDB error:", error);
+});
 
 // Demo API keys
 const apiKeys = [
